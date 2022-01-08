5d18d6551ea87dfdd2e026a3185af606c41b158f
Paweł Hincka <pawel@hincka.pl>
2022-01-08 19:44:04 +0100

    Finised about us + contact + gallery

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -8,65 +8,29 @@
 
 function postTypes()
 {
-  $sales_args = array(
+  $trips_args = array(
     'public' => true,
-    'label'  => 'Promocje',
+    'label'  => 'Wycieczki',
     'supports' => array( 'title', 'thumbnail'),
     'show_in_nav_menus' => true,
-    'rewrite' => array( 'slug' => 'promocje'),
+    'rewrite' => array( 'slug' => 'wycieczki'),
   );
-  register_post_type( 'sales', $sales_args );
+  register_post_type( 'trips', $trips_args );
 
 
-  register_taxonomy(  
-    'sales-category',  
-    'sales',  
-      array(  
-        'hierarchical' => true,
-        'labels' => array('name' => 'Lokalizacja', 'add_new_item' => __( 'Dodaj nową lokalizacje' )),
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'query_var' => true,
-        'rewrite' => array( 'slug' => 'kategoria', 'hierarchical' => true),
-      )  
-  );  
+  // register_taxonomy(  
+  //   'trips-category',  
+  //   'trips',  
+  //     array(  
+  //       'hierarchical' => true,
+  //       'labels' => array('name' => 'Wycieczki', 'add_new_item' => __( 'Dodaj nową lokalizacje' )),
+  //       'show_ui' => true,
+  //       'show_admin_column' => true,
+  //       'query_var' => true,
+  //       'rewrite' => array( 'slug' => 'wycieczka', 'hierarchical' => true),
+  //     )  
+  // );  
 
-
-  $products_args = array(
-    'public' => true,
-    'label'  => 'Produkty',
-    'supports' => array( 'title', 'thumbnail'),
-    'show_in_nav_menus' => true,
-    'rewrite' => array( 'slug' => 'produkty'),
-  );
-  register_post_type( 'products', $products_args );
-
-
-  register_taxonomy(  
-    'products-city',  
-    'products',  
-      array(  
-        'hierarchical' => true,
-        'labels' => array('name' => 'Lokalizacja', 'add_new_item' => __( 'Dodaj nową lokalizacje' )),
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'query_var' => true,
-        'rewrite' => array( 'slug' => 'miasto', 'hierarchical' => true),
-      )  
-  );  
-
-  register_taxonomy(  
-    'products-category',  
-    'products',  
-      array(  
-        'hierarchical' => true,
-        'labels' => array('name' => 'Kategoria', 'add_new_item' => __( 'Dodaj nową kategorie' )),
-        'show_ui' => true,
-        'show_admin_column' => true,
-        'query_var' => true,
-        'rewrite' => array( 'slug' => 'kateogria', 'hierarchical' => true),
-      )  
-  );  
 }
 add_action('init', 'postTypes', 0);
 
